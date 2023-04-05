644f36bb8bd60a387fa84b51052991d5bca79acf
CheolWoong Choi <bluesky4381@naver.com>
2023-04-05 23:06:27 +0900

    fix : k진수에서 소수 개수 구하기 답안 개선

--- programmers/Lv2/k진수에서 소수 개수 구하기.ts
+++ programmers/Lv2/k진수에서 소수 개수 구하기.ts
@@ -1,9 +1,8 @@
 function isPrime(n: number) {
-  if (n === 1) return false;
+  if (n < 2) return false;
   if (n === 2) return true;
 
   const max = Math.floor(Math.sqrt(n));
-  let cnt = 0;
 
   for (let i = 2; i <= max; i++) {
     if (n % i === 0) return false;
@@ -14,12 +13,7 @@ function isPrime(n: number) {
 
 function solution(n: number, k: number) {
   const num = Number(n).toString(k);
-  const arr = num.split("0").filter((n) => n);
-  let answer = 0;
-
-  arr.forEach((n) => {
-    if (isPrime(+n)) answer += 1;
-  });
+  const answer = num.split("0").filter((n) => isPrime(+n)).length;
 
   return answer;
 }
