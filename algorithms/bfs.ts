6514a6333e5f7b7d8c99cbfba6758c07ba3ae7a9
CheolWoong Choi <bluesky4381@naver.com>
2023-05-08 14:47:02 +0900

    algorithm : BFS 알고리즘 구현

---
+++ algorithms/bfs.ts
@@ -0,0 +1,26 @@
+{
+  function bfs(graph: number[][], v: number, visited) {
+    const queue = [v];
+    visited[v] = true;
+
+    while (queue.length) {
+      const v = queue.shift();
+      console.log(v);
+
+      for (const i of graph[v]) {
+        if (!visited[i]) {
+          queue.push(i);
+          visited[i] = true;
+        }
+      }
+    }
+  }
+
+  const graph = [[], [2, 3, 4], [1, 5], [1, 6, 7], [1], [2], [3, 7], [3, 6]];
+  const visited = new Array(graph.length).fill(false);
+
+  // console.log(graph);
+  // console.log(visited);
+
+  bfs(graph, 1, visited);
+}
--- algorithms/dfs.ts
+++ algorithms/dfs.ts
@@ -1,20 +1,22 @@
-function dfs(graph: number[][], v: number, visited: boolean[]) {
-  visited[v] = true;
+{
+  function dfs(graph: number[][], v: number, visited: boolean[]) {
+    visited[v] = true;
 
-  console.log(v, " ");
+    console.log(v, " ");
 
-  for (const i of graph[v]) {
-    if (!visited[i]) {
-      dfs(graph, i, visited);
+    for (const i of graph[v]) {
+      if (!visited[i]) {
+        dfs(graph, i, visited);
+      }
     }
   }
+
+  const graph = [[], [2, 3, 4], [1, 5], [1, 6, 7], [1], [2], [3, 7], [3, 6]];
+
+  const visited = new Array(graph.length).fill(false);
+
+  // console.log(graph);
+  // console.log(visited);
+
+  dfs(graph, 1, visited);
 }
-
-const graph = [[], [2, 3, 4], [1, 5], [1, 6, 7], [1], [2], [3, 7], [3, 6]];
-
-const visited = new Array(graph.length).fill(false);
-
-// console.log(graph);
-// console.log(visited);
-
-dfs(graph, 1, visited);
