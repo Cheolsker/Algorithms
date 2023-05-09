079b5ab16de075cbd9c7d8be3f4819f37c0f7f89
CheolWoong Choi <bluesky4381@naver.com>
2023-05-09 13:36:54 +0900

    solve : 음료수 얼려먹기 코드 Develop

--- practice/[DFS|BFS] 음료수 얼려먹기.ts
+++ practice/[DFS|BFS] 음료수 얼려먹기.ts
@@ -1,45 +1,45 @@
 // DFS
 {
-  function dfs(
-    graph: number[][],
-    visited: boolean[][],
-    pos: { x: number; y: number },
-    limit: { n: number; m: number }
-  ) {
-    const { x, y } = pos;
-    const { n, m } = limit;
+  function solution() {
+    // dfs
+    function dfs(x: number, y: number) {
+      if (x < 0 || x >= ROW || y < 0 || y >= COLUMN) {
+        return false;
+      }
 
-    if (x < 0 || x >= n || y < 0 || y >= m) {
+      if (CUP[x][y] === 0) {
+        CUP[x][y] = 1;
+
+        dfs(x + 1, y);
+        dfs(x - 1, y);
+        dfs(x, y + 1);
+        dfs(x, y - 1);
+
+        return true;
+      }
+
       return false;
     }
 
-    if (visited[x][y] || graph[x][y] === 1) {
-      return false;
-    }
+    // 입력값
+    const ROW = 4;
+    const COLUMN = 5;
+    const CUP = [
+      [0, 0, 1, 1, 0],
+      [0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0],
+    ];
 
-    if (!visited[x][y]) {
-      visited[x][y] = true;
-
-      dfs(graph, visited, { x: x + 1, y }, limit);
-      dfs(graph, visited, { x: x - 1, y }, limit);
-      dfs(graph, visited, { x, y: y + 1 }, limit);
-      dfs(graph, visited, { x, y: y - 1 }, limit);
-    }
-
-    return true;
-  }
-
-  function solution(n: number, m: number, cup: number[][]) {
     let answer = 0;
-    const visited = Array.from(new Array(n), (_) => new Array(m).fill(false));
 
-    for (let i = 0; i < cup.length; i++) {
-      for (let j = 0; j < cup[i].length; j++) {
+    for (let i = 0; i < CUP.length; i++) {
+      for (let j = 0; j < CUP[i].length; j++) {
         // console.log("[x][y] : " + `${i}|${j}`);
-        // console.log(visited);
+        // console.log(CUP);
         // console.log("answer : " + answer);
 
-        if (dfs(cup, visited, { x: i, y: j }, { n, m })) {
+        if (dfs(i, j)) {
           answer += 1;
         }
       }
@@ -49,12 +49,5 @@
   }
 
   // test
-  const cup = [
-    [0, 0, 1, 1, 0],
-    [0, 0, 0, 1, 1],
-    [1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0],
-  ];
-
-  console.log(solution(4, 5, cup));
+  console.log(solution());
 }
