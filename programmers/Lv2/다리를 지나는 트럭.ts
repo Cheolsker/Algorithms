122a97cdcc2bab47bf334a3809faa3b1e041b98d
CheolWoong Choi <bluesky4381@naver.com>
2023-07-05 12:25:42 +0900

    fix : 프로그래머스 Lv2 - 다리를 지나는 트럭 코드 개선 - while문이 돌 때마다 매번 트럭들의 무게의 합을 구하지 않는다. - 트럭이 다리를 지나거나, 새로운 트럭이 다리를 지날 때 무게를 빼주거나 더해줌

--- programmers/Lv2/다리를 지나는 트럭.ts
+++ programmers/Lv2/다리를 지나는 트럭.ts
@@ -14,11 +14,9 @@ function solution(
 
   while (pass_trucks.length || truck_weights.length) {
     time += 1;
-    pass_trucks_weights = 0;
 
     pass_trucks.forEach((truck) => {
       truck.length += 1;
-      pass_trucks_weights += truck.weight;
     });
 
     if (pass_trucks?.[0]?.length > bridge_length) {
@@ -32,13 +30,22 @@ function solution(
         length: 1,
       };
 
-      pass_trucks.push(truck as any);
+      pass_trucks.push(truck as Truck);
+      pass_trucks_weights += truck.weight as number;
     }
   }
 
   return time;
 }
 
+console.time("sol-1");
 console.log(solution(2, 10, [7, 4, 5, 6])); // 8
+console.timeEnd("sol-1");
+
+console.time("sol-2");
 console.log(solution(100, 100, [10])); // 101
+console.timeEnd("sol-2");
+
+console.time("sol-3");
 console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])); // 110
+console.timeEnd("sol-3");
