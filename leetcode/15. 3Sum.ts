65786ff79688ce85e6af44e2f42988358cc2e497
CheolWoong Choi <bluesky4381@naver.com>
2024-04-11 17:15:00 +0900

    leetcode : 3Sum 문제 해결 보완 (정렬) - 투포인터를 사용

--- leetcode/15. 3Sum.ts
+++ leetcode/15. 3Sum.ts
@@ -1,34 +1,29 @@
-function binarySearch(array: number[], target: number, start: number) {
-  let left = start;
-  let right = array.length - 1;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-
-    if (array[mid] === target) return mid;
-    else if (array[mid] > target) right = mid - 1;
-    else left = mid + 1;
-  }
-}
-
 function threeSum(nums: number[]): number[][] {
   nums.sort((a, b) => a - b);
 
   const answer = [];
   const length = nums.length;
 
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length - 2; i++) {
     if (i > 0 && nums[i - 1] === nums[i]) continue;
 
-    const target = -nums[i];
+    let j = i + 1;
+    let k = length - 1;
 
-    for (let j = i + 1; j < length; j++) {
-      if (j > i + 1 && nums[j - 1] === nums[j]) continue;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
 
-      const idx = binarySearch(nums, target - nums[j], j + 1);
+      if (sum === 0) {
+        answer.push([nums[i], nums[j], nums[k]]);
 
-      if (idx) {
-        answer.push([nums[i], nums[j], nums[idx]]);
+        while (j < k && nums[j] === nums[j + 1]) j++;
+        while (j < k && nums[k] === nums[k + 1]) k--;
+        j++;
+        k--;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        j++;
       }
     }
   }
