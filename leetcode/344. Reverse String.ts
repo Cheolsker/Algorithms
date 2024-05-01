066606f33428b8dbd3265072afc3fc4596684ffa
CheolWoong Choi <bluesky4381@naver.com>
2024-05-01 20:43:58 +0900

    leetcode : #344 Reverse String 코드 보완

--- leetcode/344. Reverse String.ts
+++ leetcode/344. Reverse String.ts
@@ -1,7 +1,21 @@
 {
+  /**
+   * 내장 함수 사용
+   */
   function reverseString(s: string[]): void {
     s.reverse();
   }
+
+  /**
+   * 직접 구현한 풀이
+   * 
+  function reverseString(s: string[]): void {
+    for(let i=0; i<s.length/2; i++) {
+        [s[i], s[s.length-i-1]] = [s[s.length-i-1], s[i]];
+    } 
+  };
+   *
+  **/
 
   /**
    * 테스트 케이스
