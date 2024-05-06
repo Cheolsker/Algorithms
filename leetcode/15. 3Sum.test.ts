d23a428ca6d26271194c42801a2c4ca6204035c1
CheolWoong Choi <bluesky4381@naver.com>
2024-05-06 21:37:49 +0900

    leetcode : #15 3Sum 문제 다시 풂 + 테스트 코드 작성

---
+++ leetcode/15. 3Sum.test.ts
@@ -0,0 +1,92 @@
+import { threeSum1, threeSum2 } from "./15. 3Sum";
+
+const getRandomNums = (length: number) => {
+  const min = -1e5;
+  const max = 1e5;
+
+  return Array.from(
+    { length },
+    () => Math.floor(Math.random() * (max - min + 1)) + min
+  );
+};
+
+const nums100 = getRandomNums(100);
+const nums1000 = getRandomNums(1000);
+const nums1500 = getRandomNums(1500);
+const nums3000 = getRandomNums(3000);
+
+type Cases<NUMS, EXPECTED> = [NUMS, EXPECTED][];
+
+const cases: Cases<number[], number[][]> = [
+  [
+    [-1, 0, 1, 2, -1, -4],
+    [
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ],
+  ],
+  [[0, 1, 1], []],
+  [[0, 0, 0, 0], [[0, 0, 0]]],
+  [nums100, threeSum2(nums100)],
+  [nums1000, threeSum2(nums1000)],
+  [nums1500, threeSum2(nums1500)],
+  // [nums3000, threeSum2(nums3000)],
+];
+
+const performanceTable: {
+  input: string;
+  threeSum1: number;
+  threeSum2: number;
+  best: string;
+}[] = [];
+
+describe("3Sum", () => {
+  context("3Sum 1", () => {
+    it.each(cases)("case %#", (nums, expected) => {
+      expect(threeSum1(nums)).toEqual(expect.arrayContaining(expected));
+    });
+  });
+
+  context("3Sum 2", () => {
+    it.each(cases)("case %#", (nums, expected) => {
+      expect(threeSum2(nums)).toEqual(expect.arrayContaining(expected));
+    });
+  });
+
+  context("performance 비교", () => {
+    it.each(cases)("case %#", (nums, expected) => {
+      const startTime1 = performance.now();
+      const result1 = threeSum1(nums);
+      const endTime1 = performance.now();
+      const time1 = endTime1 - startTime1;
+
+      const startTime2 = performance.now();
+      const result2 = threeSum2(nums);
+      const endTime2 = performance.now();
+      const time2 = endTime2 - startTime2;
+
+      let best;
+      switch (Math.min(time1, time2)) {
+        case time1:
+          best = "threeSum1";
+          break;
+        case time2:
+          best = "threeSum2";
+          break;
+        default:
+          best = "threeSum1";
+      }
+
+      performanceTable.push({
+        input: nums.length + "개",
+        threeSum1: time1,
+        threeSum2: time2,
+        best,
+      });
+    });
+  });
+
+  afterAll(() => {
+    console.table(performanceTable);
+  });
+});
--- leetcode/15. 3Sum.ts
+++ leetcode/15. 3Sum.ts
@@ -1,29 +1,67 @@
-function threeSum(nums: number[]): number[][] {
+/**
+ * 완전탐색으로 해결
+ * 시간초과 발생
+ *
+ * 시간복잡도 : O(N^3)
+ */
+export function threeSum1(nums: number[]): number[][] {
   nums.sort((a, b) => a - b);
-
   const answer = [];
-  const length = nums.length;
 
-  for (let i = 0; i < length - 2; i++) {
+  for (let i = 0; i < nums.length - 2; i++) {
     if (i > 0 && nums[i - 1] === nums[i]) continue;
 
-    let j = i + 1;
-    let k = length - 1;
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      if (j > i + 1 && nums[j - 1] === nums[j]) continue;
 
-    while (j < k) {
-      const sum = nums[i] + nums[j] + nums[k];
+      for (let k = j + 1; k < nums.length; k++) {
+        if (k > j + 1 && nums[k - 1] === nums[k]) continue;
 
-      if (sum === 0) {
-        answer.push([nums[i], nums[j], nums[k]]);
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          answer.push([nums[i], nums[j], nums[k]]);
+        }
+      }
+    }
+  }
 
-        while (j < k && nums[j] === nums[j + 1]) j++;
-        while (j < k && nums[k] === nums[k + 1]) k--;
-        j++;
-        k--;
-      } else if (sum > 0) {
-        k--;
-      } else {
-        j++;
+  return answer;
+}
+
+/**
+ * nums를 정렬한 후, 반복문을 돌면서
+ * 투포인터를 이용해서 문제해결
+ *
+ * 시간복잡도 : O(N) * O(N) = O(N^2)
+ */
+export function threeSum2(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b);
+
+  let l = 0,
+    r = 0,
+    sum = 0;
+  const answer = [];
+
+  // O(N)만큼 반복문 실행
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i - 1] === nums[i]) continue;
+
+    l = i + 1;
+    r = nums.length - 1;
+
+    // O(N)만큼 투포인터를 이용한 로직 실행
+    while (l < r) {
+      sum = nums[i] + nums[l] + nums[r];
+
+      if (sum < 0) l++;
+      else if (sum > 0) r--;
+      else {
+        answer.push([nums[i], nums[l], nums[r]]);
+
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
+
+        l++;
+        r--;
       }
     }
   }
