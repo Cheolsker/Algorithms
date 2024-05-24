a75f0c20c0c5ea746e34b3d037505ed83799a022
CheolWoong Choi <bluesky4381@naver.com>
2024-05-24 16:26:10 +0900

    leetcode : #21. Merge Two Sorted Lists 기존 문제풀이를 개선한 풀이 추가

--- leetcode/21. Merge Two Sorted Lists.ts
+++ leetcode/21. Merge Two Sorted Lists.ts
@@ -38,7 +38,7 @@
       list2 = list2.next;
     }
 
-    // O(L*log(N))
+    // O(L*log(L))
     stack.sort((a, b) => a - b);
 
     if (!stack.length) return null;
@@ -63,5 +63,46 @@
 
       return head;
     }
+  }
+
+  /**
+   * 위의 풀이를 최적화한 방법
+   *
+   * 공간복잡도를 사용하지 않고,
+   * while 문을 이용해서 두 연결리스트의 값을 비교한 후,
+   * 하나의 연결리스트에 병합 후 반환.
+   */
+  function mergeTwoLists2(list1: ListNode | null, list2: ListNode | null) {
+    const root = new ListNode(0);
+    let tail = root;
+
+    while (list1 || list2) {
+      // list1이 없으면, tail에 list2를 모두 연결
+      if (!list1) {
+        tail.next = list2;
+        list2 = null;
+      }
+      // list2가 없으면, tail에 list1을 모두 연결
+      else if (!list2) {
+        tail.next = list1;
+        list1 = null;
+      }
+      // list1과 list2의 값을 비교해서 작은 값의 노드 하나를 tail에 연결
+      else {
+        if (list1.val <= list2.val) {
+          tail.next = list1;
+          list1 = list1.next;
+
+          tail = tail.next;
+        } else {
+          tail.next = list2;
+          list2 = list2.next;
+
+          tail = tail.next;
+        }
+      }
+    }
+
+    return root.next;
   }
 }
