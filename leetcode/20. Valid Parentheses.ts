00287f39a5fbb9e2de1c6f836de794674639d3e4
CheolWoong Choi <bluesky4381@naver.com>
2024-05-27 20:48:49 +0900

    leetcode : #20. Valid Parentheses 문제 풀이

---
+++ leetcode/20. Valid Parentheses.ts
@@ -0,0 +1,29 @@
+/**
+ * 스택을 이용해서, 유효한 괄호인지 체크
+ *
+ * 시간복잡도: O(N), 공간복잡도: S(N)
+ */
+export function isValid(s: string): boolean {
+  if (s.length % 2 === 1) return false;
+
+  const stack: string[] = [];
+
+  const openBrackets = ['(', '{', '['];
+  const pairs = new Map([
+    [')', '('],
+    ['}', '{'],
+    [']', '['],
+  ]);
+
+  for (const w of s) {
+    if (openBrackets.includes(w)) {
+      stack.push(w);
+    } else {
+      if (pairs.get(w) !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
