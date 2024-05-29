083049c5e80c987d211e341d6c7e03538712ab6a
CheolWoong Choi <bluesky4381@naver.com>
2024-05-29 16:04:17 +0900

    leetcode : #232. Implement Queue using Stacks 문제 풀이, 책 풀이 코드 추가

--- leetcode/232. Implement Queue using Stacks.ts
+++ leetcode/232. Implement Queue using Stacks.ts
@@ -3,7 +3,19 @@ import { NodeItem } from '../algorithms/data_structure/node';
 
 const SIZE = 150;
 
-class MyQueue {
+/**
+ * 나의 풀이
+ *
+ * 2개의 스택을 사용
+ *
+ * push() : O(M) + O(1)
+ * 기존 스택의 값들을 pop한 후, rev 연결리스트에 연결
+ * 연결리스트를 돌면서, 값들을 새로운 스택에 넣고, 새로운 스택을 this.stack에 연결
+ *
+ * pop() : O(1)
+ * 항상 top의 값을 꺼냄
+ */
+class MyQueue1 {
   stack: MyStack<number>;
 
   constructor() {
@@ -46,5 +58,53 @@ class MyQueue {
 
   empty(): boolean {
     return this.stack.isEmpty();
+  }
+}
+
+/**
+ * 책 풀이
+ *
+ * 평균 시간복잡도가 O(1)
+ * push() : O(1)
+ *
+ * pop() : 평균 시간복잡도 O(1) -> O(N) + (O(1) + O(1) + ...) / N
+ * output이 empty일 때, M개 요소가 input에 있으면, POP시 O(M) + O(1)
+ * output이 empty가 아니면, O(1)만큼 시간복잡도 발생
+ */
+class MyQueue2 {
+  input: MyStack<number>;
+  output: MyStack<number>;
+
+  constructor() {
+    this.input = new MyStack(SIZE);
+    this.output = new MyStack(SIZE);
+  }
+
+  push(x: number): void {
+    this.input.push(new NodeItem(x));
+  }
+
+  pop(): number {
+    const top = this.peek();
+
+    return this.output.pop()?.item ?? -1;
+  }
+
+  peek(): number {
+    if (this.output.isEmpty()) {
+      while (!this.input.isEmpty()) {
+        const last = this.input.pop();
+
+        if (last) {
+          this.output.push(last);
+        }
+      }
+    }
+
+    return this.output.top?.item ?? -1;
+  }
+
+  empty(): boolean {
+    return this.input.isEmpty() && this.output.isEmpty();
   }
 }
