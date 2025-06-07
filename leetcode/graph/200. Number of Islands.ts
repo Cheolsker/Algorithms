a08f12abde56b3cf03f38cd872078128be9d2081
CheolWoong Choi <bluesky4381@naver.com>
2025-06-07 21:30:37 +0900

    leetcode: 200. Number of Islands 2차풀이 - 코드정리

--- leetcode/graph/200. Number of Islands.ts
+++ leetcode/graph/200. Number of Islands.ts
@@ -1,3 +1,4 @@
+// 첫번째 풀이
 {
   function numIslands(grid: string[][]): number {
     const rowLength = grid.length;
@@ -38,5 +39,46 @@
     }
 
     return true;
+  }
+}
+
+/**
+ * 두번째 풀이
+ * - 코드정리
+ */
+{
+  function DFS(grid: string[][], row: number, column: number) {
+    if (
+      row < 0 ||
+      row > grid.length - 1 ||
+      column < 0 ||
+      column > grid[row].length - 1 ||
+      grid[row][column] === "0"
+    ) {
+      return false;
+    }
+
+    grid[row][column] = "0";
+
+    DFS(grid, row - 1, column);
+    DFS(grid, row + 1, column);
+    DFS(grid, row, column - 1);
+    DFS(grid, row, column + 1);
+
+    return true;
+  }
+
+  function numIslands(grid: string[][]): number {
+    let count = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (DFS(grid, i, j)) {
+          count++;
+        }
+      }
+    }
+
+    return count;
   }
 }
